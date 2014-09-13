32da5ac1531e0557942f033b13fe0fde3b7ed913
sekkithub <sekkithub@gmail.com>
2014-09-13 20:44:09 +0000

    add line_comments = false on  config.rb

--- config.rb
+++ config.rb
@@ -1,5 +1,7 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
+asset_cache_buster :none
+
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -12,10 +14,10 @@ javascripts_dir = "js"
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
