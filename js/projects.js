d394e9d671a4a432301f3e9dd2284e73428cd3a0
sekkithub <sekkithub@gmail.com>
2014-09-15 00:22:22 +0000

    layout projects list on CSS

--- js/projects.js
+++ js/projects.js
@@ -7,42 +7,42 @@ function ProjectsCtrl($scope) {
     $scope.projects = [
         {
             date: '19/08/2014',
-            title: '<a href="" target="_blank">MIX YOUTUBE IN THE HOUSE</a>',
+            title: '<a href="http://mixyoutubeinthehouse.com/" target="_blank">MIX YOUTUBE IN THE HOUSE</a>',
             tag: '<span class="www">WWW</span>' + ', ' + '<span class="app">Application</span>'
         },
         {
             date: '05/07/2014',
-            title: '<a href="" target="_blank">MELODY WALKS</a>',
+            title: '<a href="https://vimeo.com/100014968" target="_blank">MELODY WALKS</a>',
             tag: '<span class="instalation">Instalation</span>'
         },
         {
             date: '23/06/2014',
-            title: '<a href="" target="_blank">DIGITAL ARK</a>',
+            title: '<a href="https://vimeo.com/97481244" target="_blank">DIGITAL ARK</a>',
             tag: 'Project' + ', ' + 'Video'
         },
         {
             date: '16/05/2014',
-            title: '<a href="" target="_blank">aquaptic</a>',
+            title: '<a href="https://vimeo.com/94885880" target="_blank">aquaptic</a>',
             tag: 'Instalation'
         },
         {
             date: '14/05/2014',
-            title: '<a href="" target="_blank">MA Interactive Media 13-14 Work in Progress Show</a>',
+            title: '<a href="http://wip14.int-des.com/" target="_blank">MA Interactive Media 13-14 Work in Progress Show</a>',
             tag: 'WWW'
         },
         {
             date: '02/05/2014',
-            title: '<a href="" target="_blank">ATELIER KEIICHI MATSUDA</a>',
+            title: '<a href="http://km.cx/" target="_blank">ATELIER KEIICHI MATSUDA</a>',
             tag: 'WWW'
         },
         {
             date: '12/09/2013',
-            title: '<a href="" target="_blank">@kiki</a>',
+            title: '<a href="http://www.hitomihitomi.com/" target="_blank">@kiki</a>',
             tag: 'WWW'
         },
         {
             date: '06/11/2012',
-            title: '<a href="" target="_blank">Alexander Soloviev</a>',
+            title: '<a href="http://alexandersoloviev.co.uk/" target="_blank">Alexander Soloviev</a>',
             tag: 'WWW'
         }
     ];
